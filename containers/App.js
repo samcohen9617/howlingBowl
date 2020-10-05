b9a86b27cb2b97703e82ff74650c5e87fd9d882b
Sam Cohen <samcohen96@gmail.com>
2020-10-05 13:26:01 -0400

    Most things working except setMessage not getting recognized

--- containers/App.js
+++ containers/App.js
@@ -1,7 +1,17 @@
 import React, {Component} from 'react';
-
-export default class App extends Component {
-    render () {
-        return <p>This is my new react app</p>
+import InputPreview from '../components/InputPreview';
+import {connect} from 'react-redux';
+class App extends Component {
+    _onChange = (value) => {
+      this.props.dispatch(setMessage(value))
     }
-}
+    render () {
+      const {message} = this.props.messageReducer;
+      return (
+         <InputPreview
+           value={message}
+           onChange={this._onChange}/>
+      )
+    }
+  }
+  export default connect(state => state)(App);
