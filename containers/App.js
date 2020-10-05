277cc2f47bd469dc53045d38de0fd810ce754707
Sam Cohen <samcohen96@gmail.com>
2020-10-05 14:44:33 -0400

    Finished part 2 from the Manoj blog

--- containers/App.js
+++ containers/App.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import InputPreview from '../components/InputPreview';
 import {connect} from 'react-redux';
+import {setMessage} from '../actions/message';
+import {Link} from 'react-router-dom';
+
 class App extends Component {
     _onChange = (value) => {
       this.props.dispatch(setMessage(value))
@@ -8,9 +11,14 @@ class App extends Component {
     render () {
       const {message} = this.props.messageReducer;
       return (
+        <div>
          <InputPreview
            value={message}
            onChange={this._onChange}/>
+          <Link to="/about">
+           <button>Go to About</button>
+          </Link>
+        </div>
       )
     }
   }
